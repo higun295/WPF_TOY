36a256d1d444983c2f1cceae044fcc31b423dee5
HEEKWON <higun295@gmail.com>
2023-11-22 16:20:52 +0900

    20231122_2

---
+++ WPF_Toy.Forms/UI/Units/ShakableTextBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Toy.Forms.UI.Units
+{
+    public class ShakableTextBox : TextBox
+    {
+        static ShakableTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ShakableTextBox), new FrameworkPropertyMetadata(typeof(ShakableTextBox)));
+        }
+    }
+}
